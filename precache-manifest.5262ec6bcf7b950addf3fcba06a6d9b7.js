ef96ab29644b93998b749df1d80e504c3ec5056c
EzequielCaste <ezequiel.caste@gmail.com>
2020-10-01 20:51:44 -0300

    Updates

---
+++ precache-manifest.5262ec6bcf7b950addf3fcba06a6d9b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d44a139d1f805b0b9523f65c045194",
+    "url": "/react-gif-search/index.html"
+  },
+  {
+    "revision": "21b6050e6d48067cc62f",
+    "url": "/react-gif-search/static/css/main.4ee7e6d3.chunk.css"
+  },
+  {
+    "revision": "53ea673a37f0a3151a78",
+    "url": "/react-gif-search/static/js/2.4bc76726.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gif-search/static/js/2.4bc76726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b6050e6d48067cc62f",
+    "url": "/react-gif-search/static/js/main.82803316.chunk.js"
+  },
+  {
+    "revision": "25341864f8be733ff455",
+    "url": "/react-gif-search/static/js/runtime-main.3bf38e5b.js"
+  }
+]);
